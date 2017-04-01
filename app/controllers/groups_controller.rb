c63d5b86a8070563d0192a64fa424d9025e20a39
lixuan310 <lixuan19900214@gmail.com>
2017-04-01 16:06:06 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_and_check_permission, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -31,7 +31,6 @@ class GroupsController < ApplicationController
   end
 
   def update
-
     find_group_and_check_permission
 
     if @group.update(group_params)
